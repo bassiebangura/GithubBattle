ba0c0cee4e8e70915ddd033c5a63700bf73bdf7f
Bassie Bangura <bassiebangura@Bassies-MacBook-Pro.local>
2020-05-06 13:21:37 -0400

    Add theme to app.

--- app/index.js
+++ app/index.js
@@ -1,21 +1,36 @@
-import React, {Component} from 'react'
-import ReactDOM from 'react-dom'
-import './index.css'
-import Popular from './components/Popular';
-import Battle from './components/battle'
+import React, { Component } from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import Popular from "./components/Popular";
+import Battle from "./components/battle";
+import Nav from "./components/nav";
+import { ThemeProvider } from "./contexts/theme";
 
 class App extends Component {
-    render(){
-        return (
-            <div className='container'>
-              <Battle />
-                {/* <Popular /> */}
-            </div>
-        )
-    }
+	constructor(props) {
+		super(props);
+		this.state = {
+			theme: "light",
+			toggleTheme: () => {
+				this.setState(({ theme }) => ({
+					theme: theme === "light" ? "dark" : "light"
+				}));
+			}
+		};
+	}
+	render() {
+		return (
+			<ThemeProvider value={this.state}>
+				<div className={this.state.theme}> 
+					<div className="container">
+						<Nav />
+						{/* <Battle /> */}
+						<Popular />
+					</div>
+				</div>
+			</ThemeProvider>
+		);
+	}
 }
 
-ReactDOM.render(
-    <App/>,
-    document.getElementById("app")
-)
+ReactDOM.render(<App />, document.getElementById("app"));
