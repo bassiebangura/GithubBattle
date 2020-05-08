0e29eef40f9f999c9be9fd08241bfeb5a6df0c61
Bassie Bangura <bassiebangura@Bassies-MacBook-Pro.local>
2020-05-08 08:15:14 -0400

    Apply coding splitting with React.lazy()

--- app/index.js
+++ app/index.js
@@ -2,24 +2,28 @@ import React, { Component } from "react";
 import ReactDOM from "react-dom";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import "./index.css";
-import Popular from "./components/Popular";
-import Battle from "./components/Battle";
-import Results from "./components/Results";
+// import Popular from "./components/Popular";
+// import Battle from "./components/Battle";
+// import Results from "./components/Results";
 import NoMatch from "./components/NoMatch";
+import Loading from "./components/Loading";
 import Nav from "./components/Nav";
 import { ThemeProvider } from "./contexts/theme";
 
+const LazyBattle = React.lazy(() => import("./components/Battle"));
+const LazyResults = React.lazy(() => import("./components/Results"));
+const LazyPopular = React.lazy(() => import("./components/Popular"));
+
 class App extends Component {
-	
-		state = {
-			theme: "light",
-			toggleTheme: () => {
-				this.setState(({ theme }) => ({
-					theme: theme === "light" ? "dark" : "light"
-				}));
-			}
-		};
-	
+	state = {
+		theme: "light",
+		toggleTheme: () => {
+			this.setState(({ theme }) => ({
+				theme: theme === "light" ? "dark" : "light"
+			}));
+		}
+	};
+
 	render() {
 		return (
 			<Router>
@@ -28,9 +32,12 @@ class App extends Component {
 						<div className="container">
 							<Nav />
 							<Switch>
-								<Route exact path="/" component={Popular} />
-								<Route exact path="/battle" component={Battle} />
-								<Route path="/battle/results" component={Results} />
+								<React.Suspense fallback={<Loading text="Loading" />}>
+									<Route exact path="/" component={LazyPopular} />
+									<Route exact path="/battle" component={LazyBattle} />
+									<Route exact path="/battle/results" component={LazyResults} />
+								</React.Suspense>
+
 								<Route component={NoMatch} />
 							</Switch>
 						</div>
