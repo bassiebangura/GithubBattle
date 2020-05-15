1d0960790fb879f6300bf732931d94075c3a360b
Bassie Bangura <bassiebangura@Bassies-MacBook-Pro.local>
2020-05-15 14:36:55 -0400

    Implement useEffect

--- app/components/Loading.js
+++ app/components/Loading.js
@@ -1,36 +1,31 @@
-import React, {useState, useEffect} from 'react'
-import PropTypes from 'prop-types'
-
+import React, { useState, useEffect } from "react";
+import PropTypes from "prop-types";
 
 const styles = {
-  content: {
-    fontSize: '35px',
-    position: 'absolute',
-    left: '0',
-    right: '0',
-    marginTop: '20px',
-    textAlign: 'center',
-  }
+	content: {
+		fontSize: "35px",
+		position: "absolute",
+		left: "0",
+		right: "0",
+		marginTop: "20px",
+		textAlign: "center"
+	}
+};
+export default function Loading({ text = "Loading", speed = 3000 }) {
+	const [content, setContent] = useState(text);
+
+	useEffect(() => {
+		const id = window.setInterval(
+			() => setContent(content === `${text}...` ? content : `${text}.`),
+			speed
+		);
+
+		return () => window.clearInterval(id);
+	}, [text, speed]);
+	return <p style={styles.content}>{content}</p>;
 }
-export default function Loading({text='Loading', speed=3000}) {
-    const [content, setContent] = useState(text)
-
-    useEffect(()=> {
-        const id = window.setInterval(() => (
-            setContent(content => (
-                content === `${content}...` ? content : `${content}.`
-            ))), speed)
-
-
-        return () => window.clearInterval(id)
-    }, [text, speed])
-
-}
-
 
 Loading.propTypes = {
-    text: PropTypes.string,
-    speed: PropTypes.number
-}
-
-
+	text: PropTypes.string,
+	speed: PropTypes.number
+};
