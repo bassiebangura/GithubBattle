0c191deabc8871720bc975505a10947280c6b09c
Bassie Bangura <bassiebangura@Bassies-MacBook-Pro.local>
2020-05-15 11:23:22 -0400

    Convert App from class to functional comp

--- app/index.js
+++ app/index.js
@@ -1,10 +1,8 @@
-import React, { Component } from "react";
+import React, { Component, useState } from "react";
 import ReactDOM from "react-dom";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import "./index.css";
-// import Popular from "./components/Popular";
-// import Battle from "./components/Battle";
-// import Results from "./components/Results";
+
 import NoMatch from "./components/NoMatch";
 import Loading from "./components/Loading";
 import Nav from "./components/Nav";
@@ -14,38 +12,31 @@ const LazyBattle = React.lazy(() => import("./components/Battle"));
 const LazyResults = React.lazy(() => import("./components/Results"));
 const LazyPopular = React.lazy(() => import("./components/Popular"));
 
-class App extends Component {
-	state = {
-		theme: "light",
-		toggleTheme: () => {
-			this.setState(({ theme }) => ({
-				theme: theme === "light" ? "dark" : "light"
-			}));
-		}
-	};
+function App() {
+	const [theme, setTheme] = useState("light");
+	const toggleTheme = () =>
+		setTheme(theme => (theme === "light" ? "dark" : "light"));
 
-	render() {
-		return (
-			<Router>
-				<ThemeProvider value={this.state}>
-					<div className={this.state.theme}>
-						<div className="container">
-							<Nav />
-							<Switch>
-								<React.Suspense fallback={<Loading text="Loading" />}>
-									<Route exact path="/" component={LazyPopular} />
-									<Route exact path="/battle" component={LazyBattle} />
-									<Route exact path="/battle/results" component={LazyResults} />
-								</React.Suspense>
+	return (
+		<Router>
+			<ThemeProvider value={theme}>
+				<div className={theme}>
+					<div className="container">
+						<Nav toggleTheme={toggleTheme} />
+						<Switch>
+							<React.Suspense fallback={<Loading text="Loading" />}>
+								<Route exact path="/" component={LazyPopular} />
+								<Route exact path="/battle" component={LazyBattle} />
+								<Route exact path="/battle/results" component={LazyResults} />
+							</React.Suspense>
 
-								<Route component={NoMatch} />
-							</Switch>
-						</div>
+							<Route component={NoMatch} />
+						</Switch>
 					</div>
-				</ThemeProvider>
-			</Router>
-		);
-	}
+				</div>
+			</ThemeProvider>
+		</Router>
+	);
 }
 
 ReactDOM.render(<App />, document.getElementById("app"));
